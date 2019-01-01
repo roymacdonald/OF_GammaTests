540a6db79ea7b34d8ed827f8134298e9f7b5dfe5
Roy Macdonald <macdonald.roy@gmail.com>
2019-01-01 06:01:00 -0300

    updated documentation

--- shared_code/src/gamma_tests.h
+++ shared_code/src/gamma_tests.h
@@ -2,6 +2,17 @@
 #include "ofMain.h"
 
 //---------------------------------------------------------------------------------------------
+
+/**
+computes and returns a vector with a discretized gausian distribution.
+
+ @param mu this is the mean of the distribution, it sets where the center of the distribution will be. Normally just use zero. 
+ @param sigma This is like the width of the gaussian bell. If you use sigma = 1 then the bell will "reach" zero around 3.1 and -3.1. I say "reach" because it will not really reach zero but when this distribution is represented in 8 bits (as used for images) it is at this point where it becomes zero.
+ @param width How far away the distribution gets. use 3.1 in this parameter if you want it to reach zero when you convert it into 8 bits.
+ @param numSamples The number of samples used you'll get back.
+ @param bNormalize If set to true the summ of all the values will be one.
+ @return This distribution is returned as a vector of vec3s. The x value is the position at which it is being sampled this distribution. y has the value of the gaussian curve. If you are just interested on the gaussian distribution values, just use the y component of each of the returned elements. z is always zero.
+ */
 inline std::vector<glm::vec3> getGaussianDistribution(float mu, float sigma, float width, size_t numSamples, bool bNormalize = false){
 	
 	float s2 = 2.0 * sigma * sigma;
@@ -27,6 +38,14 @@ inline std::vector<glm::vec3> getGaussianDistribution(float mu, float sigma, flo
 	return gaussian;
 }
 //---------------------------------------------------------------------------------------------
+/**
+ Returns a 2d kernel with a gaussian distribution.
+ Parameters for gaussian distribution have been adjusted so that the edges of the kernel are always zero.
+
+ @param kernelSize size of the kernel, the kernel is square so this applies for width and height
+ @param bNormalize when set to true it will normalize the kernel so the sum of all of its values are 1.
+ @return a 2 dimension vector of floats with the kernel. 
+ */
 inline std::vector<std::vector<float> > getGaussianKernel(size_t kernelSize, bool bNormalize = true){
 	//when bNormalize is set to true the sum of all the kernel elements will be 1
 	
@@ -52,25 +71,60 @@ inline std::vector<std::vector<float> > getGaussianKernel(size_t kernelSize, boo
 	return kernel;
 }
 //---------------------------------------------------------------------------------------------
+/**
+ Decodes the gamma from some pixels with gamma encoded pixels, which is usually always the case, into pixels in a linear color space.
+ It decodes inplace, so it modiefies the pixels passed.
+
+ @param pix The pixels to decode.
+ @param gamma the gamma to decode. The default is 2.2 which is the most common value.
+ */
 inline void decodeGamma(ofPixels& pix, float gamma = 2.2){
 	auto px = pix.getData();
 	for(size_t i = 0; i < pix.size(); i++){
 		px[i] = pow(px[i]/255.0, gamma) *255;
 	} 
 }
+/**
+ Decodes the gamma from an image with gamma encoded pixels, which is usually always the case, into pixels in a linear color space.
+ It decodes inplace, so it modiefies the image passed. after modifying the pixels it will update the texture of the image.
+
+ @param pix The image to decode.
+ @param gamma the gamma to decode. The default is 2.2 which is the most common value.
+ */
 inline void decodeGamma(ofImage& img, float gamma = 2.2){
 	decodeGamma(img.getPixels(), gamma); 
 	img.update();
 }
 //---------------------------------------------------------------------------------------------
+/**
+ Encode  some pixels with pixels in linear space, into pixels in a logarithmic color space.
+ It encodes inplace, so it modiefies the pixels passed.
+
+ @param pix The pixels to encode.
+ @param gamma the gamma to encode. The default is 2.2 which is the most common value.
+ 
+ */
 inline void encodeGamma(ofPixels& pix, float gamma = 2.2){
 	decodeGamma(pix, 1/gamma);
 }
+/**
+ Encode  some pixels with pixels in linear space, into pixels in a logarithmic color space.
+ It encodes inplace, so it modiefies the image passed. After modifying the pixels it will update the texture of the image.
+
+ @param pix The image to encode.
+ @param gamma the gamma to encode. The default is 2.2 which is the most common value.
+ */
 inline void encodeGamma(ofImage& img, float gamma = 2.2){
 	encodeGamma(img.getPixels(), gamma);
 	img.update();
 }
 //---------------------------------------------------------------------------------------------
+/**
+ Draws the test pattern
+
+ @param brightness sets the max brightness of the pattern.
+ @param saturation sets the max saturation of the pattern.
+ */
 inline void drawGammaTestPattern(float brightness = 1.0f, float saturation = 1.0f){
 
 	std::vector<ofColor> colors = {
@@ -109,6 +163,13 @@ inline void drawGammaTestPattern(float brightness = 1.0f, float saturation = 1.0
 	
 }
 //---------------------------------------------------------------------------------------------
+/**
+ renders the testpattern into an ofImage.
+
+ @param img the target image; where the pattern will be stored
+ @param brightness sets the max brightness of the pattern.
+ @param saturation sets the max saturation of the pattern.
+  */
 void renderTestPatternToImg(ofImage & img, float brightness = 1.0f, float saturation = 1.0f){
 	ofFbo fbo;
 	fbo.allocate(ofGetWidth(), ofGetHeight(),GL_RGBA32F_ARB);
